a8fc0a4cfaf8c127b02021007c587a296ab2ead5
POURMHERE <103439512+pourmhere@users.noreply.github.com>
2024-07-06 04:09:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+var modal = document.querySelector(".modal");
+var btn = document.querySelector("#openModal");
+var close = document.querySelector(".close-modal");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+close.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
